07a2a178be6a89135d5655f2b90a1cf179c53196
linshengxi <linshengxi@bingsn.com>
2021-06-09 18:01:54 +0800

    二分法查找

---
+++ src/main/java/cn/lsx/algorithm/search/BinarySearch.java
@@ -0,0 +1,58 @@
+package cn.lsx.algorithm.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 二分法查找
+ * @author linShengxi
+ * @date 2021/6/9
+ */
+
+public class BinarySearch {
+
+    public List search(int[] arr, int target) {
+        List targetIndexs = new ArrayList();
+        int left=0;
+        int right = arr.length-1;
+        while (left<right) {
+            int mid = (left+right)/2;
+            if (arr[mid] < target) {
+                 left = mid+1;
+            }else if (arr[mid] > target) {
+                right = mid-1;
+            } else {
+                targetIndexs.add(mid);
+                while (arr[--mid] == target) {
+                    targetIndexs.add(mid);
+                }
+                while (arr[++mid] == target) {
+                    targetIndexs.add(mid);
+                }
+                break;
+            }
+        }
+       // 递归 binarySearch(arr,0,arr.length,target,targetIndexs);
+        return targetIndexs;
+    }
+
+    private void binarySearch(int[] arr,int left ,int right,int target,List targetIndexs) {
+        if (left>=right) {
+            return;
+        }
+        int mid = (left + right) / 2;
+        if (arr[mid] < target) {
+            binarySearch(arr,mid+1,right,target,targetIndexs);
+        }else if (arr[mid] > target) {
+            binarySearch(arr,left,mid-1,target,targetIndexs);
+        } else {
+            targetIndexs.add(mid);
+            while (arr[--mid] == target) {
+                targetIndexs.add(mid);
+            }
+            while (arr[++mid] == target) {
+                targetIndexs.add(mid);
+            }
+        }
+    }
+}
---
+++ src/test/java/cn/lsx/algorithm/search/Search.java
@@ -0,0 +1,35 @@
+package cn.lsx.algorithm.search;
+
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author linShengxi
+ * @date 2021/6/9
+ */
+
+public class Search {
+    private int[] build() {
+        return new int[]{1,2,2,2,3,4,5,5,6,7,8,9,10};
+    }
+    private void show(List list) {
+        if (list.isEmpty()) {
+            System.out.println("没有查找到");
+            return;
+        }
+        for (Object o : list) {
+            System.out.print(o+",");
+        }
+    }
+
+    @Test
+    public void binarySearch() {
+        int[] arr = build();
+
+        BinarySearch search = new BinarySearch();
+
+        List list = search.search(arr, 5);
+        show(list);
+    }
+}
