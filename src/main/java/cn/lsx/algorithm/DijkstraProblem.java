598109cbcb971a9a24474d9fc62bf7ff4c68a2d2
linshengxi <linshengxi@bingsn.com>
2021-08-05 18:03:46 +0800

    迪杰斯特拉算法初始化

---
+++ src/main/java/cn/lsx/algorithm/DijkstraProblem.java
@@ -0,0 +1,23 @@
+package cn.lsx.algorithm;
+
+/**
+ * 迪杰斯特拉算法
+ *
+ *         A - - - 5 - - - B
+ *       -   -            -   -
+ *     7       2       3         9
+ *   -           -    -           -
+ * C                G                D
+ *   -           -    -           -
+ *     8       4        6      4
+ *        -   -           -   -
+ *          E - - - 5 - - - F
+ * 6个外卖小哥从G点出发 分别把外卖送到ABCDEF6个点
+ * 计算G到其他点最短距离
+ * 其他点到各个点最短距离又是多少
+ * @author linShengxi
+ * @date 2021/8/5
+ */
+
+public class DijkstraProblem {
+}
