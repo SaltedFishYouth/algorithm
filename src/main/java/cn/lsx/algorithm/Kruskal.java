5497f55881174b2b0786a831f46ddf3c00bd840c
linshengxi <linshengxi@bingsn.com>
2021-08-05 17:57:58 +0800

    克鲁斯卡尔算法完成

--- src/main/java/cn/lsx/algorithm/Kruskal.java
+++ src/main/java/cn/lsx/algorithm/Kruskal.java
@@ -1,5 +1,11 @@
 package cn.lsx.algorithm;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.*;
+
 /**
  * 克鲁斯卡尔算法
  *
@@ -36,6 +42,107 @@ public class Kruskal {
         Kruskal kruskal = new Kruskal(vertexs,matrix);
 
         kruskal.show();
+        kruskal.calculate();
+
+    }
+
+    private void calculate() {
+        //创建边
+        List<Edge> edgeList = buildEdge();
+        //对边进行从小大打排序
+        bubbleSort(edgeList);
+
+        //节点的链接的最大节点 当两个节点的 最大节点相同时 就不能相连会构成回路
+        int[] edgeEndVertex = new int[vertexs.length];
+        for (int i = 0; i < edgeEndVertex.length; i++) {
+            edgeEndVertex[i] = i;
+        }
+        List<Edge> result = new LinkedList<>();
+
+        for (int i = 0; i < edgeList.size(); i++) {
+            Edge edge = edgeList.get(i);
+            //最大节点相同 构成回路 不可以相连
+            int startVertex = getEnds(edgeEndVertex,edge.start);
+            int endVertex = getEnds(edgeEndVertex,edge.end);
+
+            if (startVertex == endVertex) {
+                continue;
+            }
+
+            int maxEndVertex = startVertex>endVertex?startVertex:endVertex;
+//            edgeEndVertex[edge.start] = maxEndVertex;
+            setEnds(edgeEndVertex,edge.start,maxEndVertex);
+            result.add(edge);
+        }
+
+        System.out.println("打印结果");
+        for (Edge edge : result) {
+            System.out.println(edge);
+        }
+    }
+
+    /**
+     * description:获取节点的最大节点
+     *
+     * @param edgeEndVertex
+     * @param i
+     * @return int
+     */
+    private int getEnds(int[] edgeEndVertex ,int i){
+
+        while (edgeEndVertex[i] != i) {
+            i = edgeEndVertex[i];
+        }
+        return i;
+    }
+    /**
+     * description:获取节点的最大节点
+     *
+     * @param edgeEndVertex
+     * @param i
+     * @return int
+     */
+    private int setEnds(int[] edgeEndVertex ,int i,int end){
+
+        while (edgeEndVertex[i] != i) {
+            i = edgeEndVertex[i];
+            edgeEndVertex[i] = end;
+        }
+        edgeEndVertex[i] = end;
+        return i;
+    }
+
+    private List<Edge> buildEdge() {
+        int length = vertexs.length;
+        List<Edge> edgeList = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                int weight = matrix[i][j];
+                if (weight == NO || weight==0) {
+                    continue;
+                }
+                edgeList.add(new Edge(i,j,weight));
+            }
+        }
+        return edgeList;
+    }
+
+    private void bubbleSort(List<Edge> list) {
+        //如果某一次排序时，没有发生交换说明已经排序完成无须接着排序
+        boolean okFlag = true;
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 1; j < list.size() - i; j++) {
+                if (list.get(j - 1).weight > list.get(j).weight) {
+                    Edge temp = list.get(j - 1);
+                    list.set(j - 1, list.get(j));
+                    list.set(j, temp);
+                    okFlag = false;
+                }
+            }
+            if (okFlag) {
+                return;
+            }
+        }
     }
 
     /**
@@ -75,11 +182,11 @@ public class Kruskal {
     }
 
     class Edge {
-        String start;
-        String end;
+        int start;
+        int end;
         int weight;
 
-        public Edge(String start, String end, int weight) {
+        public Edge(int start, int end, int weight) {
             this.start = start;
             this.end = end;
             this.weight = weight;
@@ -87,7 +194,7 @@ public class Kruskal {
 
         @Override
         public String toString() {
-            return "Edge{<"  + start + "," + end + ">=" + weight +"}";
+            return "Edge{<"  + vertexs[start] + "," + vertexs[end] + ">=" + weight +"}";
         }
     }
 
